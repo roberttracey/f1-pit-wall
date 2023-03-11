8671c8482b672e5a12f382e29c7f1afb4c474fab
RobertTracey <robertpatricktracey@gmail.com>
2023-03-11 16:31:09 +0000

    method comments

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request, redirect, url_for, send_from_
 import os
 app = Flask(__name__)
 
-
+# use this method to return to home page
 @app.route('/')
 def index():
    print('Request for index page received')
@@ -14,16 +14,19 @@ def favicon():
     return send_from_directory(os.path.join(app.root_path, 'static'),
                                'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
+# use this method to go to schedule pahe
 @app.route('/schedule', methods=['POST'])
 def schedule():
    print('Navigate to schedule.html')
    return render_template('schedule.html')
 
+# use this method to return to standing page
 @app.route('/standing', methods=['POST'])
 def standing():
    print('Navigate to standing.html')
    return render_template('standing.html')
 
+# use this method to return to simulate page
 @app.route('/simulate', methods=['POST'])
 def simulate():
    print('Navigate to simulate.html')
